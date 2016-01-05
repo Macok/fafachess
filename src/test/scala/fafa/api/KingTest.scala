23c2cbf5417d16e1f78601cfcadaf561152a54ae
mac <maciej.pocwierz@gmail.com>
2016-01-05 18:28:20 +0100

    KingTest

---
+++ src/test/scala/fafa/api/KingTest.scala
@@ -0,0 +1,50 @@
+package fafa.api
+
+import fafa.api.Board._
+import fafa.api.Role.{King, Queen}
+
+/**
+  * Created by mac on 04.01.16.
+  */
+class KingTest extends BaseTest {
+
+  "Knight" should "threat pieces next to it" in {
+    val actor =
+      """
+        |
+        |
+        |
+        |
+        |pp
+        |K p
+        |P
+        |
+      """ actorAt Pos.A3 get
+
+    actor.role should be(King)
+
+    capturingMoves(actor) should be(Set(
+      Move(Pos.A3, Pos.A4, capturing = Some(Pos.A4)),
+      Move(Pos.A3, Pos.B4, capturing = Some(Pos.B4))
+    ))
+  }
+
+  it should "move to any position next to it" in {
+    val actor =
+      """
+        |
+        |
+        |
+        |
+        |pp
+        |K p
+        |P
+        |
+      """ actorAt Pos.A3 get
+
+    nonCapturingMoves(actor) should be(Set(
+      Move(Pos.A3, Pos.B3),
+      Move(Pos.A3, Pos.B2)
+    ))
+  }
+}
